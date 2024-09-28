bc80b4c488f8309f828064ec451f7d7c4f26e25f
Balderrama <sbalderrama3@miners.utep.edu>
2024-09-28 16:42:53 -0600

    basic call to MW API and prints output to browser

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from urllib.request import urlopen
+from dotenv import load_dotenv
+import os
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_from_api():
+    load_dotenv()
+    API_KEY = os.getenv('MW_COLLEGIATE_API_KEY')
+    print(API_KEY, file=sys.stderr)
+    with urlopen(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/hello?key={API_KEY}') as r:
+        text = r.read()
+    return text
